f2778bb134061af86ad3d59f4371b39859d86d2c
cravethedave <d.saikali@live.com>
2025-12-11 19:52:48 -0500

    Optimized the code using sparse matrix properties, 60x faster

--- 25/day_9/star_2/solve_v3.py
+++ 25/day_9/star_2/solve_v3.py
@@ -21,6 +21,8 @@ off my math.
 
 As I mention earlier, there is a small error in this, but it could be fixed with one step of 
 pre-processing where we only draw the outermost edge of the shape.
+
+After a bit of extra work, the process is 60 times faster, taking 40 seconds total.
 """
 
 import time
@@ -39,7 +41,7 @@ start = time.time()
 lowest_x, lowest_y = np.min(corners, axis=0)
 highest_x, highest_y = np.max(corners, axis=0) + 1
 corners -= np.array([lowest_x,lowest_y])
-matrix = np.zeros((highest_x-lowest_x, highest_y-lowest_y), dtype=np.int8)
+matrix = lil_matrix((highest_x-lowest_x, highest_y-lowest_y), dtype=np.int8)
 
 # Initialize the edge of the matrix
 for i, current in enumerate(corners):
@@ -52,33 +54,35 @@ for i, current in enumerate(corners):
     else:
         for j in range(min(y1,y2), max(y1,y2) + 1):
             matrix[x1,j] = 1
-print(f"Initialized the sides of the matrix after {time.time() - start}s")
-print(matrix.shape)
-matrix = lil_matrix(matrix)
-print(f"Converted to sparse matrix after {time.time() - start}s")
+
+# Convert to a csr matrix to optimize future operations
+matrix = matrix.tocsr()
 
 # Iterate over all corner pairs
 biggest_rectangle = 0
 for i, v1 in enumerate(corners):
-    print(i,v1,biggest_rectangle)
+    # print(i,v1,biggest_rectangle)
     x1,y1 = v1
     for j,v2 in enumerate(corners[i+1:]):
         x2,y2 = v2
-        
+
         # Special case for thin rectangles, avoids issues with raycast later
         if abs(x1-x2) <= 1 or abs(y1-y2) <= 1:
             area = prod(np.abs(v2-v1) + 1)
             biggest_rectangle = max(biggest_rectangle, area)
-        
+            continue
+
         frst_x = min(x1,x2)
         last_x = max(x1,x2)
         frst_y = min(y1,y2)
         last_y = max(y1,y2)
 
         # The inside of the rectangle cannot contain any edges
-        if np.sum(matrix[frst_x+1:last_x, frst_y+1:last_y]) != 0:
+        if matrix[frst_x+1:last_x, frst_y+1:last_y].getnnz() != 0:
             continue
+
         # Diagonal raycast to count intersections with the edges
+        checkpoint = time.time()
         x_range = []
         y_range = []
         x_start = int((frst_x + last_x)//2)
@@ -91,7 +95,7 @@ for i, v1 in enumerate(corners):
         # An even amount of hits indicates we are outside of the shape
         if np.sum(matrix[x_range,y_range]) % 2 == 0:
             continue
-                    
+
         # Rectangle is valid, find area
         area = prod(np.abs(v2-v1) + 1)
         biggest_rectangle = max(biggest_rectangle, area)
